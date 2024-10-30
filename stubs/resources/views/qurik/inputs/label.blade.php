438890f363469dbc6594d56ed714af204cc307d1
end3r-man <praveen.venkatachalam@proton.me>
2024-10-30 07:21:19 +0530

    🏛️ Class Up

---
+++ stubs/resources/views/qurik/inputs/label.blade.php
@@ -0,0 +1,3 @@
+<div {{$attributes->merge(['class' => 'label'])}}>
+    <span class="label-text">{{$slot}}</span>
+</div>
--- stubs/resources/views/qurik/inputs/password.blade.php
+++ stubs/resources/views/qurik/inputs/password.blade.php
@@ -1,12 +1,15 @@
-@props(['icon' => null])
+@props([
+    'icon' => null,
+    'class' => null
+])
 
-<div x-data="{open:false}" class="input input-bordered flex items-center gap-x-2">
+<div x-data="{ open: false }" class="w-full input input-bordered flex items-center gap-x-2 {{$class}}">
 
     @if ($icon)
-        <span class="{{$icon}} text-xl"></span>
+        <span class="{{ $icon }} text-xl"></span>
     @endif
 
-    <input :type="open ? 'text':'password'" type="password" class="grow">
+    <input {{ $attributes }} :type="open ? 'text' : 'password'" type="password" class="grow w-full">
 
-    <span @click="open = !open" :class="open ? 'solar--eye-bold':'solar--eye-closed-bold'" class="iconify text-xl"></span>
+    <span @click="open = !open" :class="open ? 'solar--eye-bold' : 'solar--eye-closed-bold'" class="iconify text-xl"></span>
 </div>
